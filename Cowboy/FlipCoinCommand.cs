b00a5f9f68f19720c3039b2df90b4f0093db9d62
collenirwin <collenirwin@gmail.com>
2021-12-30 04:09:05 -0500

    add FlipCoinCommand

---
+++ Cowboy/FlipCoinCommand.cs
@@ -0,0 +1,27 @@
+﻿using Discord;
+using Discord.WebSocket;
+
+namespace Cowboy;
+
+internal class FlipCoinCommand : ISlashCommand
+{
+    public string Name => "flip-coin";
+
+    public string Description => "Flips a coin (heads or tails)";
+
+    public async Task ExecuteAsync(SocketSlashCommand command)
+    {
+        var result = Random.Shared.Next(2) == 0 ? "Heads" : "Tails";
+        var message = $"Coin flip:{Environment.NewLine}> **{result}**";
+
+        await command.RespondAsync(text: message);
+    }
+
+    public SlashCommandProperties Build()
+    {
+        return new SlashCommandBuilder()
+            .WithName(Name)
+            .WithDescription(Description)
+            .Build();
+    }
+}
