97f65ec761f9201c3359f0785acaa4d64f08f43b
collenirwin <collenirwin@gmail.com>
2022-02-04 16:45:58 -0500

    add unit test for HelpCommand

---
+++ Cowboy.UnitTests/Help.cs
@@ -0,0 +1,23 @@
+﻿using Cowboy.Commands;
+using System.Linq;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Cowboy.UnitTests;
+
+public class Help
+{
+    [Fact]
+    public async Task ContainsAllCommandsAndDescriptions()
+    {
+        var command = new HelpCommand();
+        var received = new PropReceivedCommand();
+
+        await command.ExecuteAsync(received);
+
+        Assert.Single(
+            received.Output,
+            output => output is not null &&
+                SlashCommands.All.Values.All(cmd => output.Contains(cmd.Name) && output.Contains(cmd.Description)));
+    }
+}
