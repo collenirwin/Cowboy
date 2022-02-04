8396bc0084d9a239f9bdb24608da9c1d7d7c20cc
collenirwin <collenirwin@gmail.com>
2022-02-04 16:40:39 -0500

    add unit test for FlipCoinCommand I guess

---
+++ Cowboy.UnitTests/FlipCoin.cs
@@ -0,0 +1,22 @@
+﻿using Cowboy.Commands;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Cowboy.UnitTests;
+
+public class FlipCoin
+{
+    [Fact]
+    public async Task OutputsHeadsOrTails()
+    {
+        var command = new FlipCoinCommand();
+        var received = new PropReceivedCommand();
+
+        await command.ExecuteAsync(received);
+
+        Assert.Single(
+            received.Output,
+            output => output is not null &&
+                (output.ToLower().Contains("heads") || output.ToLower().Contains("tails")));
+    }
+}
