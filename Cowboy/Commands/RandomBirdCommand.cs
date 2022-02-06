26c3bf37dc17578c0b6326c3150b26b7b25fc11e
collenirwin <collenirwin@gmail.com>
2022-02-06 13:20:45 -0500

    add /random-shibe, /random-bird, /random-cat

---
+++ Cowboy/Commands/RandomBirdCommand.cs
@@ -0,0 +1,16 @@
+﻿namespace Cowboy.Commands;
+
+/// <summary>
+/// Gets a random bird image url from the shibe.online api.
+/// </summary>
+internal class RandomShibeCommand : ShibeCommandBase
+{
+    /// <inheritdoc />
+    public override string Name => "random-bird";
+
+    /// <inheritdoc />
+    public override string Description => "Gets a link to a random bird picture";
+
+    /// <inheritdoc />
+    protected override string Endpoint => "birds";
+}
