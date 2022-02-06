4749f657db0fa5f0c10e9e2e4db4dfacbca03522
collenirwin <collenirwin@gmail.com>
2022-02-05 22:15:31 -0500

    add unit test for RandomStreetViewCommand

---
+++ Cowboy.UnitTests/RandomStreetView.cs
@@ -0,0 +1,26 @@
+﻿using Cowboy.Commands;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Cowboy.UnitTests;
+
+public class RandomStreetView
+{
+    private const string _urlPrefix = "https://www.google.com/maps/@?api=1&map_action=pano";
+
+    [Fact]
+    public async Task OutputsStreetViewUrl()
+    {
+        var command = new RandomStreetViewCommand();
+        var received = new PropReceivedCommand();
+
+        await command.ExecuteAsync(received);
+
+        Assert.Single(
+            received.Output,
+            output => output is not null &&
+                output.StartsWith(_urlPrefix) &&
+                Regex.IsMatch(output, @"&viewpoint=-?\d{1,2}\.\d+,-?1?\d{1,2}\.\d+$"));
+    }
+}
